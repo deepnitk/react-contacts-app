2ec0dd07d633538d971e0f2133a1fcb55940999f
apple <ratnadeep.konar@gmail.com>
2019-04-01 08:14:20 +0530

    add clear query

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -11,6 +11,10 @@ class ListContacts extends Component {
       query: query.trim()
     }))
   }
+
+  clearQuery = ()=> (
+    this.updateQuery('')
+  )
   render() {
     const { query } = this.state;
     const { contacts, onDeleteContact } = this.props;
@@ -31,6 +35,13 @@ class ListContacts extends Component {
             onChange = {(event)=> this.updateQuery(event.target.value)}
           />
         </div>
+        {showContacts.length !== contacts.length &&(
+          <div className = 'showing-contacts'>
+            <span>Now showing {showContacts.length} of {contacts.length}
+            </span>
+            <button onClick= {this.clearQuery}>Show all</button>
+          </div>
+        )}
         <ol className='contact-list'>
         {showContacts.map((contact) => (
           <li key={contact.id} className='contact-list-item'>
